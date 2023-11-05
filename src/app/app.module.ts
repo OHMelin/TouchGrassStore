42093ca648b07f6b6e24c36d9bfdea9450c2910c
OHMelin <oliverholstmelin@gmail.com>
2023-11-06 00:47:09 +0100

    Cleaned out
    - Deleted unnecessary components
    - New font family
    - Template section dividers
    - Removed realistic background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
-import { ShippingComponent } from './components/shipping/shipping.component';
 import { IncludedComponent } from './components/included/included.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { FaqComponent } from './components/faq/faq.component';
@@ -15,11 +12,8 @@ import { FaqComponent } from './components/faq/faq.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    HeaderComponent,
     FooterComponent,
     HeroComponent,
-    ShippingComponent,
     IncludedComponent,
     ReviewsComponent,
     FaqComponent
