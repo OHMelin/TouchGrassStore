d3d1bf501b9090a24b8e1176fcd8129652ab5fd1
OHMelin <oliverholstmelin@gmail.com>
2024-04-07 00:08:23 +0200

    Update angular CDK

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 export const environment = {
     production: false,
-    POSTHOG_KEY:'',
+    POSTHOG_KEY:process.env.POSTHOG_KEY,
     POSTHOG_HOST:'https://app.posthog.com',
 };
